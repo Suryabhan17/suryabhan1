34b5e380ce7a97878768e3acbd1855487d0ab2c8
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-21 17:28:43 +0530

    new main

--- add.java
+++ add.java
@@ -3,10 +3,13 @@ package suryabhan1;
 public  class add {
   public static void main(String[] args) {
 
+    System.out.println("1n1s");
+
     System.out.println("1n");
     System.out.println("1b");
 
-    System.out.println("1a1");
+    System.out.println("1a1d");
+
 
 
   }
